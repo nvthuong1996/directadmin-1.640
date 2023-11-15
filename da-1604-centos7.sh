55d5c9b5305f465393013116e14c648fcccb2f52
thuongnv <nvthuong1996@gmail.com>
2023-11-15 10:33:47 +0700

    update

--- da-1604-centos7.sh
+++ da-1604-centos7.sh
@@ -337,7 +337,7 @@ SCRIPTS_PATH=$DA_PATH/scripts
 PACKAGES=$SCRIPTS_PATH/packages
 SETUP=$SCRIPTS_PATH/setup.txt
 
-SERVER=https://pub-fc99108c5f144f369b91b856e0f999f4.r2.dev/services
+SERVER=http://mirrors.bkns.vn/directadmin2020
 BFILE=$SERVER/custombuild/${CB_VER}/custombuild/build
 CBPATH=$DA_PATH/custombuild
 BUILD=$CBPATH/build
@@ -922,7 +922,7 @@ if [ -s ${CB_OPTIONS} ]; then
 	if [ `grep -c '^php1_release=' ${CB_OPTIONS}` -gt 1 ]; then
 		echo "Duplicate entries found in options.conf. Likely broken. Clearing options.conf, grabbing fresh build, and trying again."
 		rm -f ${CB_OPTIONS}
-		wget -O /usr/local/directadmin/custombuild/build https://pub-fc99108c5f144f369b91b856e0f999f4.r2.dev/services/custombuild/2.0/custombuild/build
+		wget -O /usr/local/directadmin/custombuild/build http://mirrors.bkns.vn/directadmin2020/custombuild/2.0/custombuild/build
 	fi
 fi
 
@@ -1279,8 +1279,8 @@ if [ "$CURLDEV" -eq 0 ]; then
 			echo "*************************";
 			echo "* Cannot find /usr/include/curl/curl.h.  Php compile may fail. (yum -y install libcurl-devel)";
 			echo "* If yum doesn't work, install rpms from your respective OS path (use only 1):";
-			echo "*   https://pub-fc99108c5f144f369b91b856e0f999f4.r2.dev/services/es_6.0/libcurl-devel-7.19.7-16.el6.i686.rpm";
-			echo "*   https://pub-fc99108c5f144f369b91b856e0f999f4.r2.dev/services/es_6.0_64/libcurl-7.19.7-16.el6.x86_64.rpm";
+			echo "*   http://mirrors.bkns.vn/directadmin2020/es_6.0/libcurl-devel-7.19.7-16.el6.i686.rpm";
+			echo "*   http://mirrors.bkns.vn/directadmin2020/es_6.0_64/libcurl-7.19.7-16.el6.x86_64.rpm";
 			echo "*";
 			echo "* If you can install libcurl-devel quick enough in a 2nd ssh window, the php compile may work.";
 			echo "*************************";
@@ -1586,11 +1586,11 @@ if [ "$OS" = "debian" ]; then
 
 		echo "*******************";
 		echo "";
-		echo "wget does not support https.  Downloading a new wget for you from https://pub-fc99108c5f144f369b91b856e0f999f4.r2.dev/services/debian_${OS_VER}/wget";
+		echo "wget does not support https.  Downloading a new wget for you from http://mirrors.bkns.vn/directadmin2020/debian_${OS_VER}/wget";
 		echo "";
 		echo "*******************";
 
-		$BIN_DIR/wget -O $BIN_DIR/wget2 https://pub-fc99108c5f144f369b91b856e0f999f4.r2.dev/services/debian_${OS_VER}/wget
+		$BIN_DIR/wget -O $BIN_DIR/wget2 http://mirrors.bkns.vn/directadmin2020/debian_${OS_VER}/wget
 		RET=$?
 		if [ $RET -eq 0 ]; then
 			mv $BIN_DIR/wget $BIN_DIR/wget.orig
@@ -1604,7 +1604,7 @@ if [ "$OS" = "debian" ]; then
 			echo "wget does not appear to be functioning with https.";
 			echo "run the following to get a new wget binary:";
 			echo "  cd /usr/bin";
-			echo "  wget -O wget2 https://pub-fc99108c5f144f369b91b856e0f999f4.r2.dev/services/debian_${OS_VER}/wget";
+			echo "  wget -O wget2 http://mirrors.bkns.vn/directadmin2020/debian_${OS_VER}/wget";
 			echo "  mv wget wget.orig";
 			echo "  mv wget2 wget";
 			echo "  chmod 755 wget";
